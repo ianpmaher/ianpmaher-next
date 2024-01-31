52e9288cce619518f423f63ec141733a0528a232
Ian Maher <ianpmaher@gmail.com>
2024-01-30 22:54:05 -0500

    migrating styles incl styled components

---
+++ src/app/components/Button.jsx
@@ -0,0 +1,42 @@
+"use client";
+
+import styled from "styled-components"
+
+const ButtonA = styled.button`
+    font-size: 1rem;
+    min-width: 3rem;
+    min-height: 2rem;
+    max-width: 5rem;
+    font-family: "Roboto Flex", sans-serif;
+    padding: 0.25rem;
+    margin: 0.25rem auto;
+    color: #000;
+    border: 3px solid #fff;
+    outline: 3px solid #fff;
+    border-radius: 20px;
+    background-color: #FFD040;
+    cursor: pointer;
+    transition: all 0.35s ease-out;
+    &:hover {
+        opacity: 1;
+        outline: 0.25rem solid #fff;
+        transform: scale(1.1);
+    }
+    &:active {
+        background-color: rgb(105, 255, 255);
+    }
+    @media (max-width: 768px) {
+        font-size: 0.9rem;
+    }
+`
+const Button = ({children}) => {
+    return (
+        <>
+            <ButtonA>
+                {children}
+            </ButtonA>
+        </>
+    )
+}
+
+export default Button
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+    content: [
+        "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    theme: {
+        extend: {
+            // You can now use the font-sans and font-mono utility classes to apply the font to your elements.
+            fontFamily: {
+                sans: ['var(--font-karla)'],
+                roboto: ["Roboto Flex", "sans-serif"],
+                inter: ["Inter", "sans-serif"],
+            },
+            backgroundImage: {
+                gradient: "radial-gradient(circle at 100%, rgba(82,84,164,1) 10%, rgba(247,160,130,1) 40%, rgba(178,166,215,1) 55%)",
+            },
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
 };
