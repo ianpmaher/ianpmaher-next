d17eab93fea046f2d5ae4dd730124a31b3b0afcd
Ian <ianpmaher@gmail.com>
2024-03-12 21:32:19 -0400

    about

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -2,8 +2,10 @@ const AboutPage = (props) => {
     return (
         <div>
             <h1>About</h1>
+            <p>Providence College | Master of Education: Counseling — Providence, RI May 2018</p>
+            <p>Boston University | Bachelor of Arts: Psychology, English — Boston, MA May 2015</p>
         </div>
     );
-}
+};
 
 export default AboutPage;
