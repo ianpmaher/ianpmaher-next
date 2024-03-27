56664ceb7978466edb018f3c71da31ae9c91421c
Ian Maher <ianpmaher@gmail.com>
2024-03-26 21:19:25 -0400

    fixed route after build error

---
+++ src/app/keyboards/page.js
@@ -0,0 +1,11 @@
+const KeyboardPage = (props) => {
+    return (
+        <div>
+            <h1>Mechanical Keyboards</h1>
+            <p>They click and clack.</p>
+            <p>They thock.</p>
+        </div>
+    );
+};
+
+export default KeyboardPage;
