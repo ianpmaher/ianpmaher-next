abce83f69833eb06a7e401f6ae24cfd3007ae916
Ian Maher <ianpmaher@gmail.com>
2024-04-05 22:32:49 -0400

    playing w header position

--- src/app/shared/Header.jsx
+++ src/app/shared/Header.jsx
@@ -1,5 +1,7 @@
 import Contact from "../components/Contact";
+import IconCard from "../components/IconCard";
 import DarkMode from "./DarkMode";
+import Link from "next/link";
 
 const Header = (props) => {
     return (
@@ -8,11 +10,18 @@ const Header = (props) => {
                 <h1 className=" text-3xl md:text-5xl">Ian Maher</h1>
                 <h2 className=" text-2xl md:text-3xl">Full Stack Developer & Education Advocate</h2>
             </div> */}
-            <div className="absolute top-3 mx-auto my-0 flex md:flex-row gap-2">
-                <Contact />
-                <span className="relative top-0">
+            <div className="relative top-3 mx-auto my-0 flex md:flex-row justify-between gap-2">
+                <div className="flex items-center">
+                    <Link href="/" className="">
+                        <IconCard variant="text">home</IconCard>
+                    </Link>
+                </div>
+                <div className="mx-auto my-0 flex md:flex-row gap-2">
+                    <Contact />
+                    {/* <span className="relative top-0"> */}
                     <DarkMode />
-                </span>
+                    {/* </span> */}
+                </div>
             </div>
         </header>
     );
