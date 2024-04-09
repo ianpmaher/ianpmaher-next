2d7352f2e07c80afa6f56965b8a662178d0e296f
Ian Maher <ianpmaher@gmail.com>
2024-04-08 23:31:21 -0400

    trying to fix blog page format

--- src/app/blog/layout.jsx
+++
@@ -1,9 +0,0 @@
-const Layout = ({ children }) => {
-    return (
-        <div className="flex items-center justify-center h-full w-[50vw] my-0 mx-auto ">
-            <div className="w">{children}</div>
-        </div>
-    );
-};
-
-export default Layout;
--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,12 +1,12 @@
 import PostContainer from "../components/PostContainer";
 
-
 const BlogPage = () => {
-
     return (
-        <div className="flex flex-col justify-center items-center ">
-            <h1>Blog</h1>
-            <PostContainer />
+        <div>
+            <div className="flex flex-col justify-center items-center ">
+                <h1>Blog</h1>
+                <PostContainer />
+            </div>
         </div>
     );
 };
