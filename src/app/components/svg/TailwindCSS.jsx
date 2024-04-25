e30d6ed7b4af9027f0ccc1f2d89b642cb8994ecd
Ian Maher <ianpmaher@gmail.com>
2024-04-25 18:49:35 -0400

    create svg components for most logos ty figma

---
+++ src/app/components/svg/TailwindCSS.jsx
@@ -0,0 +1,32 @@
+const TailwindCSS = (props) => {
+    return (
+        <div className="flex justify-center items-center my-0 mx-auto min-h-10 max-h-40 min-w-10 max-w-40 ">
+            <svg xmlns="http://www.w3.org/2000/svg" width="113" height="68" viewBox="0 0 113 68" fill="none">
+                <g clip-path="url(#clip0_29_75)">
+                    <path
+                        d="M56.5 0C41.4922 0 32.2227 7.50649 28.25 22.5195C33.9883 15.013 40.6094 12.3636 48.1133 14.1299C52.5273 15.013 55.1758 18.1039 58.707 21.6364C64.4453 27.3766 70.625 34 84.75 34C99.7578 34 109.027 26.4935 113 11.4805C107.262 18.987 100.641 21.6364 93.1367 19.8701C88.7227 18.5455 86.0742 15.4545 82.543 11.9221C76.8047 6.62338 70.625 0 56.5 0ZM28.25 34C13.2422 34 3.97266 41.5065 0 56.5195C5.73828 49.013 12.3594 46.3636 19.8633 48.1299C24.2773 49.013 26.9258 52.1039 30.457 55.6364C36.1953 61.3766 42.375 68 56.5 68C71.5078 68 80.7773 60.4935 84.75 45.039C79.0117 52.5455 72.3906 55.6364 64.8867 53.8701C60.4727 52.5455 57.8242 49.4545 54.293 45.9221C48.5547 40.6234 42.375 34 28.25 34Z"
+                        fill="url(#paint0_linear_29_75)"
+                    />
+                </g>
+                <defs>
+                    <linearGradient
+                        id="paint0_linear_29_75"
+                        x1="-316.4"
+                        y1="2176"
+                        x2="9456.58"
+                        y2="7800.1"
+                        gradientUnits="userSpaceOnUse"
+                    >
+                        <stop stop-color="#2298BD" />
+                        <stop offset="1" stop-color="#0ED7B5" />
+                    </linearGradient>
+                    <clipPath id="clip0_29_75">
+                        <rect width="113" height="68" fill="white" />
+                    </clipPath>
+                </defs>
+            </svg>
+        </div>
+    );
+};
+
+export default TailwindCSS;
