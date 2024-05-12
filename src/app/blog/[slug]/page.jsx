128a3aac8f9842c39b4beced01ab4321b8e58460
Ian <ianpmaher@gmail.com>
2024-05-12 19:49:51 -0400

    restaurants article web scraping

--- src/app/blog/[slug]/page.jsx
+++ src/app/blog/[slug]/page.jsx
@@ -11,9 +11,9 @@ const renderers = { // react-markdown changed this to "components"
         return children;
     },
     // This custom renderer changes how strong text is rendered
-    strong: ({children}) => <strong style={{color: 'green'}}>{children}</strong>,
+    strong: ({children}) => <strong style={{fontSize: '1.4rem'}}>{children}</strong>,
     // This custom renderer changes how emphasis text is rendered
-    em: ({children}) => <em style={{color: 'purple'}}>{children}</em>,
+    em: ({children}) => <em style={{fontSize: '1.3rem'}}>{children}</em>,
     // This custom renderer changes how paragraphs are rendered
     paragraph: ({children}) => <p style={{color: 'orange'}}>{children}</p>,
 };
@@ -27,7 +27,7 @@ export default async function BlogPostPage({ params }) {
             <h1 className="text-4xl font-bold mb-4 prose">{postData.title}</h1>
             {/* <div dangerouslySetInnerHTML={{ __html: postData.content }} /> */}
             <div className="prose lg:prose-xl text-justify whitespace-pre-line">
-                <ReactMarkdown components={renderers} className="text-lg prose lg:prose-xl prose-a:underline prose-a:font-mono" >{postData.content}</ReactMarkdown>
+                <ReactMarkdown components={renderers} className="text-lg prose lg:prose-xl prose-a:underline prose-a:font-mono prose-a:text-xl hover:prose-a:text-blue-700 prose-h1:text-3xl" >{postData.content}</ReactMarkdown>
             </div>
         </div>
     );
