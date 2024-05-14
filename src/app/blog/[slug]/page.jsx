31f09989df747450a5c853d91aed7c142978c3a1
Ian <ianpmaher@gmail.com>
2024-05-13 21:02:14 -0400

    formatting for md images w max fit

--- src/app/blog/[slug]/page.jsx
+++ src/app/blog/[slug]/page.jsx
@@ -24,14 +24,14 @@ export default async function BlogPostPage({ params }) {
     const postData = await getPostData(params.slug);
 
     return (
-        <div className="px-8 py-12">
+        <div className="px-4 md:px-2 py-12">
             <h1 className="text-4xl font-bold mb-4 prose">{postData.title}</h1>
             {/* <div dangerouslySetInnerHTML={{ __html: postData.content }} /> */}
             <div className="prose lg:prose-xl text-justify whitespace-pre-line">
                 <ScrollDiv />
                 <ReactMarkdown
                     components={renderers}
-                    className="text-lg prose lg:prose-xl prose-a:underline prose-a:font-mono prose-a:text-xl hover:prose-a:text-blue-700 prose-h1:text-3xl prose-img:w-full prose-img:h-full md:prose-img:w-3/4 md:prose-img:h-3/4 "
+                    className="text-lg prose lg:prose-xl prose-a:underline prose-a:font-mono prose-a:text-xl hover:prose-a:text-blue-700 prose-h1:text-3xl prose-img:w-full prose-img:h-full md:prose-img:max-w-fit md:prose-img:h-3/4 touch-pinch-zoom active:prose-img:scale-110 "
                 >
                     {postData.content}
                 </ReactMarkdown>
