1bbf1dd1ed585077d496c6918376340978a78624
Ian Maher <ianpmaher@gmail.com>
2024-05-29 10:02:00 -0400

    framer motion for about page

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,9 +1,12 @@
+import { TypingWords } from "../components/TypingWords";
+
 const AboutPage = (props) => {
     return (
-        <div>
+        <div className="flex justify-normal flex-col mx-auto my-32">
             <h1>About</h1>
             <p>Providence College | Master of Education: Counseling — Providence, RI May 2018</p>
             <p>Boston University | Bachelor of Arts: Psychology, English — Boston, MA May 2015</p>
+            <TypingWords />
         </div>
     );
 };
