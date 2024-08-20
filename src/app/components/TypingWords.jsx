11c1f39d8c837b21606876b2d39b3ed14832e932
Ian Maher <ianpmaher@gmail.com>
2024-08-20 11:53:49 -0400

    centering css

--- src/app/components/TypingWords.jsx
+++ src/app/components/TypingWords.jsx
@@ -4,36 +4,47 @@ import { motion } from "framer-motion";
 import React from "react";
 
 export function TypingWords(props) {
-    const [displayedText, setDisplayedText] = React.useState("");
-    const [i, setI] = React.useState(0);
-    const [j, setJ] = React.useState(0);
+  const [displayedText, setDisplayedText] = React.useState("");
+  const [i, setI] = React.useState(0);
+  const [j, setJ] = React.useState(0);
 
-    React.useEffect(() => {
-        const textArr = ["software engineering", "ancient history", "computer hardware", "hiking", "dogs", "coffee", "data science", "travel"];
-        if (i < textArr.length) {
-            const typingEffect = setInterval(() => {
-                if (j < textArr[i].length) {
-                    setDisplayedText((prevText) => prevText + textArr[i][j]);
-                    setJ(j + 1);
-                } else {
-                    clearInterval(typingEffect);
-                    setTimeout(() => {
-                        setDisplayedText("");
-                        setJ(0);
-                        setI(i === textArr.length - 1 ? 0 : i + 1);
-                    }, 1000); // NUMBER TO PAUSE BETWEEN WORDS
-                }
-            }, 150); // NUMBER TO PAUSE BETWEEN LETTERS
-
-            return () => {
-                clearInterval(typingEffect);
-            };
+  React.useEffect(() => {
+    const textArr = [
+      "software engineering",
+      "ancient history",
+      "computer hardware",
+      "hiking",
+      "dogs",
+      "coffee",
+      "data science",
+      "travel",
+    ];
+    if (i < textArr.length) {
+      const typingEffect = setInterval(() => {
+        if (j < textArr[i].length) {
+          setDisplayedText((prevText) => prevText + textArr[i][j]);
+          setJ(j + 1);
+        } else {
+          clearInterval(typingEffect);
+          setTimeout(() => {
+            setDisplayedText("");
+            setJ(0);
+            setI(i === textArr.length - 1 ? 0 : i + 1);
+          }, 1000); // NUMBER TO PAUSE BETWEEN WORDS
         }
-    }, [i, j]);
+      }, 150); // NUMBER TO PAUSE BETWEEN LETTERS
 
-    return (
-        <h1 className="text-start font-display text-xl font-bold tracking-[-0.02em] drop-shadow-sm md:text-xl md:leading-[2rem]">
-            {displayedText ? displayedText : ""}
-        </h1>
-    );
+      return () => {
+        clearInterval(typingEffect);
+      };
+    }
+  }, [i, j]);
+
+  return (
+    <div className="center-center translate-x-1/2 -translate-y-1/2 max-w-max ">
+      <h1 className="text-center font-display text-xl font-bold tracking-[-0.02em] drop-shadow-sm md:text-xl md:leading-[2rem] caret-lime-400 car">
+        {displayedText ? displayedText : ""}
+      </h1>
+    </div>
+  );
 }
