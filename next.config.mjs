15396f1a38c3220b16a735ff846bfb55c4320920
ianpmaher <ianpmaher@gmail.com>
2025-09-10 12:07:33 -0400

    MongoDB and nextjs mad about inactivity

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
     styledComponents: true,
   },
   images: {
-    domains: ["http.cat", "f005.backblazeb2.com", "upload.wikimedia.org", "tex.com.tw", "m.media-amazon.com", "raw.githubusercontent.com", "i.imgur.com"],
+    domains: ["http.cat", "f005.backblazeb2.com", "upload.wikimedia.org", "tex.com.tw", "m.media-amazon.com", "raw.githubusercontent.com", "i.imgur.com", "zealpc.net"],
   },
 };
 
